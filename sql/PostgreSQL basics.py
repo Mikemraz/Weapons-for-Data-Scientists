c31c525eda45900be2028f6952536759a5729470
Liming <shazi0415@qq.com>
2019-03-22 16:27:03 -0400

    add clean data in SQL tutorial

--- sql/PostgreSQL basics.py
+++ sql/PostgreSQL basics.py
@@ -2,15 +2,10 @@ import psycopg2 as pg
 from sqlalchemy import create_engine
 import pandas as pd
 
-
+# import a pandas dataframe into postgres db.
 df = pd.read_csv('calendar.csv')
-#conn = pg.connect("dbname=postgres  user=postgres  password=jlm041544")
-#cur = conn.cursor()
-
 table_name = 'calendar'
 engine = create_engine('postgresql://postgres:jlm041544@localhost:5432/postgres')
 con = engine.connect()
-#print(engine.table_name)
 df.to_sql(table_name, engine)
-#print(engine.table_name)
 conn.commit()
